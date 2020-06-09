6e242274186e994c0e4fceebfac6931c6a2f0778
Santiago Diaz <kerbindr@gmail.com>
2020-06-09 13:20:09 -0400

    Speed up the move of the face through time

--- src/store/index.js
+++ src/store/index.js
@@ -9,11 +9,13 @@ export default new Vuex.Store({
     chances: 3,
     showModal: false,
     intervalId: 0,
+    speed: 1500,
+    changeSpeed: 0,
 
     top: 0,
     left: 0,
     x: window.innerWidth - 80,
-    y: window.innerHeight - 140,
+    y: window.innerHeight - 145,
   },
 
   mutations: {
@@ -31,21 +33,33 @@ export default new Vuex.Store({
 
     GAME_OVER(state) {
       clearInterval(state.intervalId)
+      state.speed = 1500
     },
 
     startGame(state) {
-      state.intervalId = setInterval(() => {
+      state.changeSpeed = state.speed
+      state.intervalId = setInterval(moveFace, state.speed)
+
+      function moveFace() {
         state.top = Math.floor(Math.random() * state.y) + 48
         state.left = Math.floor(Math.random() * state.x)
 
-        if (state.chances > 0) state.chances--
-        else {
+        if (state.chances > 0) {
+          state.chances--
+          state.changeSpeed -= 20
+        } else {
           clearInterval(state.intervalId)
+          state.speed = 1500
+          state.changeSpeed = state.speed
           state.showModal = !state.showModal
         }
 
-      }, 1000)
-
+        if (state.changeSpeed !== state.speed) {
+          clearInterval(state.intervalId)
+          state.speed = state.changeSpeed
+          state.intervalId = setInterval(moveFace, state.speed)
+        }
+      }
       state.score = 0
     }
   },
